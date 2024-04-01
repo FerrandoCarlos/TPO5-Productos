45b9823e3d9a70bde90aa0c4a98579c7795099f2
FerrandoCarlos <cferrando2015@gmail.com>
2024-04-01 11:56:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TPO5-Productos
+ Se realiza una tabla de Productos con java swing y mvc
---
+++ src/controlador/Pincipal.java
@@ -0,0 +1,14 @@
+
+package controlador;
+
+/**
+ * @author Ferrando Carlos
+ */
+public class Pincipal {
+
+   
+    public static void main(String[] args) {
+        
+    }
+
+}
